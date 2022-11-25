afa71dee6a3895ce59dc4ac8f6364ec626c5a918
Gords <geremy220206@gmail.com>
2022-11-25 14:57:50 -0600

    new

--- Interacciones/crear/crear de int.js
+++ Interacciones/crear/crear de int.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "cr",
-    code: `$createApplicationCommand[$guildID;int hug;Abraza a un usuario :D;true;CHAT_INPUT;{
+    code: `$createApplicationCommand[$guildID;int-hug;Abraza a un usuario :D;true;CHAT_INPUT;{
         "name": "Usuario",
         "description": "Menciona a alguien",
         "type": 6,
