428e2af3fd6746c0a04e47fec17680e93a0c1c29
Gords <geremy220206@gmail.com>
2022-11-27 17:58:21 -0600

    si

--- eventos/Status.js
+++ eventos/Status.js
@@ -2,14 +2,12 @@ module.exports = (bot) => {
 bot.status({
   text: "Astro || v$getVar[version]",
   type: "COMPETING",
-  status: "dnd",
   time: 12
 })
 
 bot.status({
   text: "$serverCount Servidores y $allMembersCount Usuarios",
   type: "WATCHING",
-  status: "dnd",
   time: 12
 })
 }
