d22b6317c143c6c7545c80924088b07e2218c413
Gords <geremy220206@gmail.com>
2022-12-02 16:12:19 -0600

    neuw

--- comandos/Diversion/hack.js
+++ comandos/Diversion/hack.js
@@ -1,34 +1,34 @@
 module.exports = {
     name: "hack",
     code: `$reply[$messageID;false]
-    Iniciando Hack!
-    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-    $editIn[3s;Hack Iniciado en $userTag[$findMember[$message]]
-    █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Conectando con el Servidor
-    ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Conectando con el Servidor
-    ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Conexion Completada con exito
-    ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Descargando Virus
-    ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Descargando Virus
-    █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Descarga Completa
-    ██████▒▒▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
-    ██████▒▒▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
-    ███████▒▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
-    ████████▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
-    █████████▒▒▒▒▒▒▒▒▒▒;Instalacion Completa!
-    ██████████▒▒▒▒▒▒▒▒▒;Obteniendo Datos de $userTag[$findMember[$message]]
-    ███████████▒▒▒▒▒▒▒▒;Datos Obtenidos con Exito
-    ████████████▒▒▒▒▒▒▒;Recopilando Informacion Obtenida
-    █████████████▒▒▒▒▒▒;Recopilando Informacion Obtenida
-    ██████████████▒▒▒▒▒;Recopilando Informacion Obtenida
-    ███████████████▒▒▒▒;Enviando Informacion a los Servidores
-    ████████████████▒▒▒;Enviando Informacion a los Servidores
-    █████████████████▒▒;Eliminando Virus
-    ██████████████████▒;Virus Elminado Ya no quedan rastros del hackeo
-    ██████████████████▒;Hackeo Completado con Exito
-    Nombre: $username[$findMember[$message]]
-    ID: $findMember[$message]
-    Token: $jsonRequest[https://some-random-api.ml/bottoken;token]
-    ███████████████████]
-    $onlyIf[$findMember[$message;false]!=$authorID;**⚠️ | No puedes hackearte a ti mismo**]
-    $onlyIf[$findMember[$message;false]!=;**⚠️ | Menciona un usuario**]`
+Iniciando Hack!
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+$editIn[3s;Hack Iniciado en $userTag[$findMember[$message]]
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Conectando con el Servidor
+██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Conectando con el Servidor
+███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Conexion Completada con exito
+████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Descargando Virus
+████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Descargando Virus
+█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒;Descarga Completa
+██████▒▒▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
+██████▒▒▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
+███████▒▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
+████████▒▒▒▒▒▒▒▒▒▒▒;Instalando Virus
+█████████▒▒▒▒▒▒▒▒▒▒;Instalacion Completa!
+██████████▒▒▒▒▒▒▒▒▒;Obteniendo Datos de $userTag[$findMember[$message]]
+███████████▒▒▒▒▒▒▒▒;Datos Obtenidos con Exito
+████████████▒▒▒▒▒▒▒;Recopilando Informacion Obtenida
+█████████████▒▒▒▒▒▒;Recopilando Informacion Obtenida
+██████████████▒▒▒▒▒;Recopilando Informacion Obtenida
+███████████████▒▒▒▒;Enviando Informacion a los Servidores
+████████████████▒▒▒;Enviando Informacion a los Servidores
+█████████████████▒▒;Eliminando Virus
+██████████████████▒;Virus Elminado Ya no quedan rastros del hackeo
+██████████████████▒;Hackeo Completado con Exito
+**Nombre:** $username[$findMember[$message]]
+**ID:** $findMember[$message]
+**Token:** $jsonRequest[https://some-random-api.ml/bottoken;token]
+███████████████████]
+$onlyIf[$findMember[$message;false]!=$authorID;**⚠️ | No puedes hackearte a ti mismo**]
+$onlyIf[$findMember[$message;false]!=;**⚠️ | Menciona un usuario**]`
 }
